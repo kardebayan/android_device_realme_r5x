0b2432dcc784492c3f130d7cf97c6a2f08779dae
Alin Jerpelea <alin.jerpelea@sony.com>
2021-06-06 16:59:57 +0700

    r5x: remove deprecated display packages
    Those packages were used by legacy devices and can be removed
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- device.mk
+++ device.mk
@@ -97,7 +97,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
