82353a0cf4c3952e0da16cc68fe979f5c86f7057
Pascoato <viniciuspascoato@gmail.com>
2021-06-09 23:34:45 -0300

    r5x: Drop libhidl & build vendor variant

--- device.mk
+++ device.mk
@@ -207,8 +207,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hidl.memory.block@1.0 \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
