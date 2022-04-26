377a225df2aeafcd3ac9d5caec9cd7421a4bf05b
bheatleyyy <sugakesshaa@gmail.com>
2022-04-25 20:56:35 -0400

    r5x: Import and build libperfmgr - power HAL taken from https://github.com/mikeNG/android_hardware_lineage_libperfmgr
    Co-Authored-By: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I2ea75af9a23ff76c6ac86b8fb2254c5930ccb2e7

--- device.mk
+++ device.mk
@@ -328,8 +328,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power.stats@1.0-service.pixel
 
 # Perf
 PRODUCT_PACKAGES += \
@@ -424,6 +424,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
     vendor/qcom/opensource/commonsys-intf/display \
     vendor/qcom/opensource/wfd-commonsys \
     vendor/nxp/opensource/pn5xx
