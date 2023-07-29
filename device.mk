8155d1ccf9353572eee08d701a7f7d2645316947
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2023-07-30 02:10:17 +0600

    r5x: Drop QC WFD support entirely

--- device.mk
+++ device.mk
@@ -506,12 +506,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libavservices_minijail.vendor \
-    vendor.display.config@2.0
-
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.biometrics.face@1.0.vendor \
