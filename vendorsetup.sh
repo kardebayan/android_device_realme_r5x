b532de8978c70a5a0427b2e5712160bb26d0e981
AmritoRock <amritokarmokar5i@gmail.com>
2023-08-17 23:16:44 +0600

    r5x: vendorsetup: Clone viperFx from my fork

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,8 @@ git clone --depth=1 https://github.com/Amritorock/vendor_realme_r5x -b Trinket v
 git clone --depth=1 https://github.com/Amritorock/kernel_realme_r5x -b OSS kernel/realme/r5x
 
 # NFC Packages
-# NFC Packages
 rm -rf vendor/nxp/opensource/commonsys/packages/apps/Nfc
 git clone --depth=1 https://github.com/ArrowOS/android_vendor_nxp_opensource_packages_apps_Nfc vendor/nxp/opensource/commonsys/packages/apps/Nfc -b arrow-13.0
+
+# Viper Fx
+git clone --depth=1 https://github.com/Amritorock/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
