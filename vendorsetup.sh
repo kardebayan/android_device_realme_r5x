83caf755b5de296ddc6a642e469298186764fc74
AmritoRock <amritokarmokar5i@gmail.com>
2023-09-25 20:39:00 +0000

    r5x: vendorsetup: Use display Hal from my fork
    also use source nfc packages

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,9 +4,9 @@ git clone --depth=1 https://github.com/Amritorock/vendor_realme_r5x -b libperf v
 # Kernel Tree
 git clone --depth=1 https://github.com/Amritorock/kernel_realme_r5x -b OSS kernel/realme/r5x
 
-# NFC Packages
-rm -rf vendor/nxp/opensource/commonsys/packages/apps/Nfc
-git clone --depth=1 https://github.com/ArrowOS/android_vendor_nxp_opensource_packages_apps_Nfc vendor/nxp/opensource/commonsys/packages/apps/Nfc -b arrow-13.0
-
 # Viper Fx
 git clone --depth=1 https://github.com/Amritorock/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+# Display Hal
+rm -rf hardware/qcom-caf/sm8150/display
+git clone --depth=1 https://github.com/Amritorock/hardware_qcom_display.git -b sm8150 hardware/qcom-caf/sm8150/display
