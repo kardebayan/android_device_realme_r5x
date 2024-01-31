7435638cebc01ff26355519878bb55de400fbb72
Zinadin Zidan <zidan44@pixelexperience.org>
2024-01-31 15:40:20 +0600

    r5x: vibrator: Call vibrator-vendor-product.mk
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I8243285efaf1161616eabb1fa4b916237a8ca67a

--- device.mk
+++ device.mk
@@ -448,11 +448,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # V4a
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
