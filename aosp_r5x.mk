2f7eda5dfc5947f79d21b8ae426b0fe776e33acc
Debayan Kar <kardebayan3@gmail.com>
2024-04-09 11:53:29 +0530

    r5x: AOSP-ify

--- aosp_r5x.mk
+++ aosp_r5x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The Project Elixir
+# Copyright (C) 2022-2024 PixelOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,15 +8,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Project-Elixir stuff
+# Inherit some common PixelOS stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5x
@@ -32,12 +28,3 @@ TARGET_VENDOR_DEVICE_NAME := r5x
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
     PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
-
-# Project-Elixir Stuff
-IS_PHONE := true
-ELIXIR_BUILD_TYPE := OFFICIAL
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_BUILD_APERTURE_CAMERA := true
