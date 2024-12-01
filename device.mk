fc5ef5618649b079d19d0e4acca7f10925aa640b
Michael Bestas <mkbestas@lineageos.org>
2024-12-02 01:29:18 +0200

    r5x: Convert rootdir to init
    Change-Id: I43ccc55b98231d984474e6bba3c9fa1266bb7351
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -157,6 +157,7 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    fstab.qcom.ramdisk \
     init.class_main.sh \
     init.oppo.fingerprints.rc \
     init.oppo.fingerprints.sh \
@@ -166,11 +167,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.target.rc \
     init.r5x.rc \
+    init.recovery.qcom.rc \
     ueventd.qcom.rc
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_r5x)
 
