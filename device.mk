4a49d7f9bb9b28a7d5326e2b1096961fc37429b2
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    r5x: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -198,6 +198,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/mmi_charging_enable)
+
 # Matlog
 TARGET_DISABLE_MATLOG := true
 
