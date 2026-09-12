bd9ffa1f904d8bfeeb3c39139be5356227c92fac
Debayan Kar <kardebayan3@gmail.com>
2026-09-13 00:22:25 +0530

    Revert "r5x: Include Dolby Atmos"
    This reverts commit 28e3d134fb932278dcb47042eb2e4a0ef72e7967.

--- device.mk
+++ device.mk
@@ -56,8 +56,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-$(call inherit-product, hardware/dolby/dolby.mk)
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
